297a78e2a3b431677cc705437d00307e0010248f
atahodzaevalibekQ <atahodzaevalibek4@gmail.com>
2026-06-16 08:53:38 +0500

    lesson1

---
+++ lesson1.py
@@ -0,0 +1,10 @@
+class Hero:
+    
+    
+    #Конструктор класса
+    def __init__(self, name, level, hp):
+        self.name = name 
+        self.level = level
+        self.hp = hp
+        
+Kirito = Hero("Kirito", 100, 100)
